25498e64aab8041ca51bc3684effb6cc6ec876b6
Mittans <mittens.au+github1@gmail.com>
2025-10-10 22:40:02 +1030

    Fix duplicate Strudel REPL. Change default track to something simplier. NPM install Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,59 +1,60 @@
 import './cors-redirect';
 import './App.css';
-import { initStrudel, note, hush } from "@strudel/web";
-import "@strudel/repl";
-import { useEffect } from "react";
+import { initStrudel, note, hush, evalScope, getAudioContext, webaudioOutput, registerSynthSounds, initAudioOnFirstClick, transpiler } from "@strudel/web";
+//import "@strudel/repl";
+import { useEffect, useRef } from "react";
 import { StrudelMirror } from '@strudel/codemirror';
-import { initAudioOnFirstClick } from '@strudel/webaudio';
-import { getAudioContext, webaudioOutput, registerSynthSounds } from '@strudel/webaudio';
-import { transpiler } from '@strudel/transpiler';
-import { evalScope } from '@strudel/core';
 import { registerSoundfonts } from '@strudel/soundfonts';
 import { drawPianoroll } from '@strudel/draw';
-import { funk42 } from './tunes';
+import { stranger_tune } from './tunes';
 
 
+export function initRepl() {
+  console.log('Effect ran only once!');
+
+  const canvas = document.getElementById('roll');
+  canvas.width = canvas.width * 2;
+  canvas.height = canvas.height * 2;
+  const drawContext = canvas.getContext('2d');
+  const drawTime = [-2, 2]; // time window of drawn haps
+
+  const editor = new StrudelMirror({
+    defaultOutput: webaudioOutput,
+    getTime: () => getAudioContext().currentTime,
+    transpiler,
+    root: document.getElementById('editor'),
+    initialCode: stranger_tune,
+    drawTime,
+    onDraw: (haps, time) => drawPianoroll({ haps, time, ctx: drawContext, drawTime, fold: 0 }),
+    prebake: async () => {
+      initAudioOnFirstClick(); // needed to make the browser happy (don't await this here..)
+      const loadModules = evalScope(
+        import('@strudel/core'),
+        import('@strudel/draw'),
+        import('@strudel/mini'),
+        import('@strudel/tonal'),
+        import('@strudel/webaudio'),
+      );
+      await Promise.all([loadModules, registerSynthSounds(), registerSoundfonts()]);
+    },
+  });
+
+  document.getElementById('play').addEventListener('click', () => editor.evaluate());
+  document.getElementById('stop').addEventListener('click', () => editor.stop());
+}
 
 export function StrudelDemo() {
 
 
-  initStrudel();
-
+  const hasRun = useRef(false);
 
   useEffect(() => {
 
-    console.log('Effect ran only once!');
-
-    const canvas = document.getElementById('roll');
-    canvas.width = canvas.width * 2;
-    canvas.height = canvas.height * 2;
-    const drawContext = canvas.getContext('2d');
-    const drawTime = [-2, 2]; // time window of drawn haps
-
-    const editor = new StrudelMirror({
-      defaultOutput: webaudioOutput,
-      getTime: () => getAudioContext().currentTime,
-      transpiler,
-      root: document.getElementById('editor'),
-      initialCode: funk42,
-      drawTime,
-      onDraw: (haps, time) => drawPianoroll({ haps, time, ctx: drawContext, drawTime, fold: 0 }),
-      prebake: async () => {
-        initAudioOnFirstClick(); // needed to make the browser happy (don't await this here..)
-        const loadModules = evalScope(
-          import('@strudel/core'),
-          import('@strudel/draw'),
-          import('@strudel/mini'),
-          import('@strudel/tonal'),
-          import('@strudel/webaudio'),
-        );
-        await Promise.all([loadModules, registerSynthSounds(), registerSoundfonts()]);
-      },
-    });
-
-    document.getElementById('play').addEventListener('click', () => editor.evaluate());
-    document.getElementById('stop').addEventListener('click', () => editor.stop());
-
+    if (!hasRun.current) {
+      hasRun.current = true;
+      initStrudel();
+      initRepl();
+    }
 
   }, []);
 
@@ -71,9 +72,6 @@ export function StrudelDemo() {
           <div id="output"></div>
         </div>
         <canvas id="roll"></canvas>
-        <p>
-          Trying clicking play
-        </p>
       </main>
     </div>
   );
