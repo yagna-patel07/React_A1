478fba45a724a0288b2e05e9fc51dda1a1870f0b
Mittans <mittens.au+github1@gmail.com>
2025-10-11 00:05:54 +1030

    Added Bootstrap styling and components for a 2x2 grid. Added some buttons (don't work yet). Added text area for preprocessing to the repl (doesn't work yet).

--- src/App.js
+++ src/App.js
@@ -8,43 +8,15 @@ import { registerSoundfonts } from '@strudel/soundfonts';
 import { drawPianoroll } from '@strudel/draw';
 import { stranger_tune } from './tunes';
 
+let globalEditor = null;
 
-export function initRepl() {
-  console.log('Effect ran only once!');
+export function Test() {
 
-  const canvas = document.getElementById('roll');
-  canvas.width = canvas.width * 2;
-  canvas.height = canvas.height * 2;
-  const drawContext = canvas.getContext('2d');
-  const drawTime = [-2, 2]; // time window of drawn haps
-
-  const editor = new StrudelMirror({
-    defaultOutput: webaudioOutput,
-    getTime: () => getAudioContext().currentTime,
-    transpiler,
-    root: document.getElementById('editor'),
-    initialCode: stranger_tune,
-    drawTime,
-    onDraw: (haps, time) => drawPianoroll({ haps, time, ctx: drawContext, drawTime, fold: 0 }),
-    prebake: async () => {
-      initAudioOnFirstClick(); // needed to make the browser happy (don't await this here..)
-      const loadModules = evalScope(
-        import('@strudel/core'),
-        import('@strudel/draw'),
-        import('@strudel/mini'),
-        import('@strudel/tonal'),
-        import('@strudel/webaudio'),
-      );
-      await Promise.all([loadModules, registerSynthSounds(), registerSoundfonts()]);
-    },
-  });
-
-  document.getElementById('play').addEventListener('click', () => editor.evaluate());
-  document.getElementById('stop').addEventListener('click', () => editor.stop());
+  document.getElementById('play').addEventListener('click', () => globalEditor.evaluate());
+  document.getElementById('stop').addEventListener('click', () => globalEditor.stop());
 }
 
-export function StrudelDemo() {
-
+export default function StrudelDemo() {
 
   const hasRun = useRef(false);
 
@@ -52,9 +24,32 @@ export function StrudelDemo() {
 
     if (!hasRun.current) {
       hasRun.current = true;
-      initStrudel();
-      initRepl();
+      (async () => {
+        await initStrudel();
+
+        globalEditor = new StrudelMirror({
+          defaultOutput: webaudioOutput,
+          getTime: () => getAudioContext().currentTime,
+          transpiler,
+          root: document.getElementById('editor'),
+          initialCode: stranger_tune,
+          prebake: async () => {
+            initAudioOnFirstClick(); // needed to make the browser happy (don't await this here..)
+            const loadModules = evalScope(
+              import('@strudel/core'),
+              import('@strudel/draw'),
+              import('@strudel/mini'),
+              import('@strudel/tonal'),
+              import('@strudel/webaudio'),
+            );
+            await Promise.all([loadModules, registerSynthSounds(), registerSoundfonts()]);
+          },
+        });
+
+      })();
+      Test()
     }
+
 
   }, []);
 
@@ -63,20 +58,49 @@ export function StrudelDemo() {
     <div>
       <h2>Strudel Demo</h2>
       <main>
-        <nav>
-          <button id="play">eval</button>
-          <button id="stop">stop</button>
-        </nav>
-        <div class="container">
-          <div id="editor"></div>
-          <div id="output"></div>
+
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+              <label for="exampleFormControlTextarea1" className="form-label">Text to preprocess:</label>
+              <textarea className="form-control" id="preprocess" rows="3"></textarea>
+            </div>
+            <div className="col-md-4">
+
+              <nav>
+                <button id="process" className="btn btn-outline-primary">Preprocess</button>
+                <button id="process_play" className="btn btn-outline-primary">Proc & Play</button>
+                <br />
+                <button id="play" className="btn btn-outline-primary">Play</button>
+                <button id="stop" className="btn btn-outline-primary">Stop</button>
+              </nav>
+            </div>
+          </div>
+          <div className="row">
+            <div className="col-md-8" style={{ maxHeight: '50vh', overflowY: 'auto' }}>
+              <div id="editor" />
+            </div>
+            <div className="col-md-4">
+              <div className="form-check">
+                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault1" defaultChecked />
+                <label className="form-check-label" for="flexRadioDefault1">
+                  p1: ON
+                </label>
+              </div>
+              <div className="form-check">
+                <input className="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
+                <label className="form-check-label" for="flexRadioDefault2">
+                  p1: HUSH
+                </label>
+              </div>
+            </div>
+          </div>
         </div>
-        <canvas id="roll"></canvas>
-      </main>
-    </div>
+
+      </main >
+    </div >
   );
 
 
 }
 
-export default StrudelDemo;
